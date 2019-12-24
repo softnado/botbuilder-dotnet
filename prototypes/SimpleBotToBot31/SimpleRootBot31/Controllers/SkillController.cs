f4d967a21d2f4b4d24995e85283e7149e558c86f
Gabo Gilabert <ggilaber@microsoft.com>
2019-12-24 13:08:24 -0500

    Added 3.1 version of simpleBotToBot

---
+++ prototypes/SimpleBotToBot31/SimpleRootBot31/Controllers/SkillController.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Skills;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Microsoft.BotBuilderSamples.SimpleRootBot31.Controllers
+{
+    /// <summary>
+    /// A controller that handles skill replies to the bot.
+    /// This example uses the <see cref="SkillHandler"/> that is registered as a <see cref="ChannelServiceHandler"/> in startup.cs.
+    /// </summary>
+    [ApiController]
+    [Route("api/skills")]
+    public class SkillController : ChannelServiceController
+    {
+        private readonly ChannelServiceHandler _handler;
+
+        public SkillController(ChannelServiceHandler handler)
+            : base(handler)
+        {
+            _handler = handler;
+        }
+
+        public override async Task<IActionResult> SendToConversationAsync(string conversationId, [FromBody] Activity activity)
+        {
+            var result = await _handler.HandleSendToConversationAsync(HttpContext.Request.Headers["Authorization"], conversationId, activity).ConfigureAwait(false);
+            return new JsonResult(result);
+        }
+
+        public override async Task<IActionResult> ReplyToActivityAsync(string conversationId, string activityId, [FromBody] Activity activity)
+        {
+            var result = await _handler.HandleReplyToActivityAsync(HttpContext.Request.Headers["Authorization"], conversationId, activityId, activity).ConfigureAwait(false);
+            return new JsonResult(result);
+        }
+    }
+}
